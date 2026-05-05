cdc7ea697a05c40f7d6588e6aa3dcd5b178c9541
Ian Wrzesinski <ian@isuffix.com>
2026-05-04 20:30:06 -0400

    Clean up `RangeMapper` impl and add errors for malformed inputs

--- crates/typst-syntax/src/node.rs
+++ crates/typst-syntax/src/node.rs
@@ -248,41 +248,56 @@ impl SyntaxNode {
 
     /// Set a synthetic span for the node and all its descendants.
     pub fn synthesize(&mut self, span: Span) {
-        self.synthesize_with(0, &mut |_| span);
+        self.synthesize_with(0, &mut |_, _| span);
     }
 
     /// Set a raw range span for each node.
     ///
     /// The range is determined by mapping the node's ranges through the given
     /// `mapper`.
-    pub fn synthesize_mapped(&mut self, id: FileId, mapper: &RangeMapper) {
-        self.synthesize_with(0, &mut |range| match mapper.map(range) {
-            Some(mapped) => Span::from_range(id, mapped),
-            None => Span::detached(),
+    ///
+    /// Returns an error with the mapper's length if it was shorter than the
+    /// length of the source text.
+    pub fn synthesize_mapped(
+        &mut self,
+        id: FileId,
+        mapper: &RangeMapper,
+    ) -> Result<(), EcoString> {
+        if mapper.total < self.len() {
+            // TODO: Should we error if not exactly equal?
+            return Err(eco_format!(
+                "text length ({}) is greater than mapper length ({})",
+                self.len(),
+                mapper.total,
+            ));
+        }
+        self.synthesize_with(0, &|offset, len| {
+            Span::from_range(id, mapper.map(offset..offset + len))
         });
+        Ok(())
     }
 
-    /// Set a custom span for each node gives its range.
+    /// Set a custom span for each node given its range.
     ///
     /// Should be called with `offset = 0` on the root node.
     fn synthesize_with(
         &mut self,
         mut offset: usize,
-        f: &mut impl FnMut(Range<usize>) -> Span,
+        map_span: &impl Fn(usize, usize) -> Span,
     ) {
         match self.node_mut() {
-            Node::Leaf(leaf) => leaf.span = f(offset..offset + leaf.text.len()),
+            Node::Leaf(leaf) => leaf.span = map_span(offset, leaf.text.len()),
             Node::Inner(inner) => {
                 let inner = Arc::make_mut(inner);
-                inner.span = f(offset..offset + inner.len);
+                inner.span = map_span(offset, inner.len);
                 inner.upper = inner.span.number();
                 for child in &mut inner.children {
-                    child.synthesize_with(offset, f);
+                    child.synthesize_with(offset, map_span);
                     offset += child.len();
                 }
             }
             Node::Error(err) => {
-                Arc::make_mut(err).error.span = f(offset..offset + err.text.len())
+                Arc::make_mut(err).error.span = map_span(offset, err.text.len())
             }
         }
     }
