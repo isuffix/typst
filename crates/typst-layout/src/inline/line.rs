da8217f8f04b55d39a6ad9347cb782b80c5ed9ba
Ian Wrzesinski <ian@isuffix.com>
2026-05-11 18:47:06 -0400

    Warn for `return_and_then`

--- crates/typst-layout/src/inline/line.rs
+++ crates/typst-layout/src/inline/line.rs
@@ -464,10 +464,8 @@ pub fn apply_shift<'a>(
             .get_ref(TextElem::font)
             .into_iter()
             .find_map(|family| {
-                world
-                    .book()
-                    .select(family.as_str(), variant(styles))
-                    .and_then(|id| world.font(id))
+                let id = world.book().select(family.as_str(), variant(styles))?;
+                world.font(id)
             })
             .map_or(*scripts.kind.default_metrics(), |f| {
                 *scripts.kind.read_metrics(f.metrics())
